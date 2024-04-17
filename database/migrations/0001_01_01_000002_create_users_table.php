89e67aee06e3b296e80c42224a16992bf6e3c50a
Setanna <joenhendrikhoelckhansen@gmail.com>
2024-04-17 10:50:20 +0200

    Updated and added migrations to match ERD v1

--- database/migrations/0001_01_01_000002_create_users_table.php
+++ database/migrations/0001_01_01_000002_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId("location_id")->constrained("locations");
+            $table->foreignId("role_id")->constrained("roles");
             $table->rememberToken();
             $table->timestamps();
         });
