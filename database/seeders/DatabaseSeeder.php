222efd21d17c05a92274fe282f0fb482c51fd1fe
Setanna <joenhendrikhoelckhansen@gmail.com>
2024-04-17 10:53:05 +0200

    Updated database seeder to add a small amount of custom dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use function Database\Seeders\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,77 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        function Role($name): void
+        {
+            $Model = new \App\Models\Role();
+            $Model->name = $name;
+            $Model->save();
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Role("user");
+        Role("moderator");
+        Role("admin");
+
+        function Location($address): void
+        {
+            $Model = new \App\Models\Location();
+            $Model->address = $address;
+            $Model->save();
+        }
+
+        Location("Munkebjergvej 130");
+        Location("Risingsvej 130");
+
+        function User($first_name, $last_name, $phone_number, $email, $password, $location_id, $role_id): void
+        {
+            $Model = new \App\Models\User();
+            $Model->first_name = $first_name;
+            $Model->last_name = $last_name;
+            $Model->phone_number = $phone_number;
+            $Model->email = $email;
+            $Model->email_verified_at = now();
+            $Model->password = Hash::make($password);
+            $Model->location_id = $location_id;
+            $Model->role_id = $role_id;
+            $Model->save();
+        }
+
+        User('John', 'Doe', '1111111111', 'JohnDoe@gmail.com','Password', 1, 1);
+        User('Jane', 'Doe', '1222222222', 'JaneDoe@gmail.com','Password', 1, 2);
+        User('Jack', 'Doe', '1233333333', 'JackDoe@gmail.com', 'Password', 1, 3);
+        User('John', 'Smith', '1234444444', 'JohnSmith@gmail.com','Password', 1, 1);
+        User('Jane', 'Smith', '1234555555', 'JaneSmith@gmail.com','Password', 1, 2);
+        User('Jack', 'Smith', '1234566666', 'JackSmith@gmail.com', 'Password', 1, 3);
+
+        function Category($name): void
+        {
+            $Model = new \App\Models\Category();
+            $Model->name = $name;
+            $Model->save();
+        }
+
+        Category('Office Supplies');
+        Category('Drinks');
+
+        function Product($name, $barcode, $category_id): void
+        {
+            $Model = new \App\Models\Product();
+            $Model->name = $name;
+            $Model->barcode = $barcode;
+            $Model->category_id = $category_id;
+            $Model->save();
+        }
+
+        Product('Mountain Dew Citrus Blast 50 cl', 5741000135525, 2);
+        Product('Office Kuglepen, 50 Stk./ 1 Pk', 7392265460013, 1);
+
+        Product::find(1)->location_products()->sync([
+            1 => ['stock' => 124, 'shelf_amount' => 22],
+            2 => ['stock' => 65, 'shelf_amount' => 1]
+        ]);
+        Product::find(2)->location_products()->sync([
+            1 => ['stock' => 4, 'shelf_amount' => 2],
+            2 => ['stock' => 0, 'shelf_amount' => 1]
         ]);
     }
 }
