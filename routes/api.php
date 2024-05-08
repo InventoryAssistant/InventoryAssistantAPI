875532ea26f6fcab1b7a43316c0457fda8ec8bae
Mathias H. D. Nielsen <mathias031001@gmail.com>
2024-05-08 12:18:56 +0200

    Removed TODO

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,6 @@ Route::group(['middleware' => ['auth:sanctum', 'ability:' . TokenEnum::UPDATE->v
     Route::put('/locations/{location}', [LocationController::class, 'update']);
     Route::put('/categories/{category}', [CategoryController::class, 'update']);
     Route::put('/user/{user}', [UserController::class, 'updateCurrentUser']);
-    // TODO make upde logged in user only
 });
 
 /* Destroy */
